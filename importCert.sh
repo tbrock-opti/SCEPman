f6ac4340de940770297121a9f9d281a6303d3d79
Tim Brock <timothy.brock@optimizely.com>
2024-10-09 12:07:55 -0700

    collect scepman url in parameters add colored output automatically download scepman ca cert and convert it

--- importCert.sh
+++ importCert.sh
@@ -1,7 +1,7 @@
 # parameters
 # $1 = full path to downloaded PFX file
 # $2 = PFX file password
-# $3 = full path to SCEPman CA cert (PEM)
+# $3 = SCEPman URL prefix (app-scepman-xxxxxxxxxxxxxxx)
 # $4 = Azure UPN (Username) of user cert is for
 
 PFX_FILE="$1"
@@ -10,36 +10,38 @@ PKI_DIR="/home/$USER/.scepman"
 KEY_FILE="scepman-client.key.pem"
 #NOENC_KEY_FILE="scepman-client-noenc.key.pem"
 CERT_FILE="scepman-client.pem"
+SCEPMAN_URL="https://$3.azurewebsites.net"
 CA_CERT_PATH="$3"
 CA_CERT_FILENAME="scepman-root.pem"
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+NC=$(tput sgr0)
 
 # verify or create pki directory
-echo "Verifying PKI directory..."
+echo -e "${GREEN}Verifying PKI directory..."
 if [ -d "$PKI_DIR" ]; then
-	echo "$PKI_DIR exists, copying CA cert..."
+	echo "$PKI_DIR exists..."
 else
-	echo "$PKI_DIR NOT found, creating..."
+	echo "${GREEN}$PKI_DIR NOT found, creating..."
 	mkdir $PKI_DIR
 fi
 
-# verify or copy CA file
-echo "Verifying CA cert..."
-if [ -f $PKI_DIR/$CA_CERT_FILENAME ]; then
-	echo "CA Cert found, skipping..."
-else
-	echo "CA Cert NOT found, copying..."
-	cp $CA_CERT_PATH $PKI_DIR/
-fi
+# get CA cert and convert it to PEM
+	echo -e "${GREEN}Downloading CA cert from SCEPman..."
+	wget -O "$PKI_DIR/scepman-root.cer" \
+		"$SCEPMAN_URL/certsrv/mscep/mscep.dll/pkiclient.exe?operation=GetCACert"
+	openssl x509 -in "$PKI_DIR/scepman-root.cer" \
+		-outform PEM -out "$PKI_DIR/scepman-root.pem"
 
 # save username (to be used in renewal script)
-echo "Saving UPN..."
+echo -e "${GREEN}Saving UPN..."
 echo $4 > $PKI_DIR/upn
 
 # save private key passphrase
 echo $PFX_PASS > $PKI_DIR/pp
 
 # extract key from PFX and encrypt with PFX password
-echo "Extracting private key..."
+echo -e "${GREEN}Extracting private key..."
 openssl pkcs12 -in $PFX_FILE -nocerts -out $PKI_DIR/$KEY_FILE -passin pass:$PFX_PASS -passout pass:$PFX_PASS
 
 # extract key from PFX and do not encrypt
@@ -47,11 +49,11 @@ openssl pkcs12 -in $PFX_FILE -nocerts -out $PKI_DIR/$KEY_FILE -passin pass:$PFX_
 #openssl pkcs12 -in $PFX_FILE -nocerts -out $PKI_DIR/$NOENC_KEY_FILE -passin pass:$PFX_PASS -passout pass:""
 
 # extract certificate from PFX
-echo "Extracting client certificate..."
+echo -e "${GREEN}Extracting client certificate..."
 openssl pkcs12 -in $1 -clcerts -nokeys -out $PKI_DIR/$CERT_FILE -passin pass:$PFX_PASS
 
 # create wifi connection
-echo "Creating Wifi Connection for Optimizely Wireless..."
+echo -e "${GREEN}Creating Wifi Connection for Optimizely Wireless..."
 nmcli c add type wifi ifname wlan0 con-name "Optimizely Internal" \
 	802-11-wireless.ssid "Optimizely Internal" \
 	802-11-wireless-security.key-mgmt wpa-eap \
